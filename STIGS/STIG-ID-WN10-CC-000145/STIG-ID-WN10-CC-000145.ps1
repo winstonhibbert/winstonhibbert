d781d72374d8cd5042e8e00fe5448fe5f79590a5
winstonhibbert <152554764+winstonhibbert@users.noreply.github.com>
2025-03-31 13:39:53 -0400

    Create STIG-ID-WN10-CC-000145.ps1

---
+++ STIGS/STIG-ID-WN10-CC-000145/STIG-ID-WN10-CC-000145.ps1
@@ -0,0 +1,53 @@
+ <#
+.SYNOPSIS
+    Users must be prompted for a password on resume from sleep (on battery).
+
+.NOTES
+    Author          : Winston Hibbert
+    LinkedIn        : www.linkedin.com/in/winston-hibbert-262a44271
+    GitHub          : ghttps://github.com/winstonhibbert
+    Date Created    : 2025-31-03
+    Last Modified   : 2025-31-03
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000145
+
+.TESTED ON
+    Date(s) Tested  : 2025-31-03
+    Tested By       : Winston Hibbert
+    Systems Tested  : Win 10
+    PowerShell Ver. : 
+
+.USAGE
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-CC-000145.ps1 
+#>
+
+# PowerShell script to require password on resume from sleep (on battery)
+
+$RegistryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Power\PowerSettings\0e796bdb-100d-47d6-a2d5-f7d2daa51f51"
+$RegistryValueName = "DCSettingIndex"
+$RegistryValue = 1
+
+# Check if the registry path exists, if not, create it
+if (-not (Test-Path -Path $RegistryPath)) {
+    Write-Host "Registry path not found. Creating path: $RegistryPath"
+    New-Item -Path $RegistryPath -Force
+}
+
+# Check if the registry value exists and if it needs to be updated
+if (Test-Path "$RegistryPath\$RegistryValueName") {
+    $currentValue = Get-ItemProperty -Path $RegistryPath -Name $RegistryValueName
+    if ($currentValue.$RegistryValueName -ne $RegistryValue) {
+        Write-Host "Registry value exists but is not set correctly. Updating value to $RegistryValue"
+        Set-ItemProperty -Path $RegistryPath -Name $RegistryValueName -Value $RegistryValue
+    } else {
+        Write-Host "Registry value is already set to $RegistryValue"
+    }
+} else {
+    Write-Host "Registry value not found. Creating value: $RegistryValueName with $RegistryValue"
+    New-ItemProperty -Path $RegistryPath -Name $RegistryValueName -Value $RegistryValue -PropertyType DWord -Force
+}
+
+Write-Host "Password prompt on resume from sleep (on battery) has been enforced successfully."
